4c755d87e54b5dcc9188fcb8eca49e9cf0dad725
g-marcin <marcin.grzmil@hotmail.com>
2022-11-29 13:16:38 +0100

    DRY rule applied
    Repetitive functions wrapped in "clickDigitBtn" and "clickOperatorBtn" functions. Code formatted.

--- script.js
+++ script.js
@@ -1,173 +1,94 @@
-// function pageLoadedHandler() {
-//     alert("Kalkulator js ver 1.0 by Marcin");    welcome
-// }
-// window.onload = pageLoadedHandler;
+let display = document.getElementById("display");
 
-const display = document.getElementById("display");
-// display.innerHTML="NaN"
+// let buttons = document.getElementsByClassName("numBtn");
 
-let buttons = document.getElementsByClassName("numBtn");
+// window.onload = init();
+let operation = {
+  first: 0,
+  second: 0,
+  res: 0,
+  mode: "",
+  sign: "",
 
-window.onload=init();
-let opp = {
-    first: 0,
-    second: 0,
-    res:0,
-    mode:"",
-    sign:"",
-
-    clear: function(){
-        display.innerHTML="";
-    },
-
-    read : function(){
-      return parseFloat(display.innerHTML)   
-    }
-
-}
-
-function checkChar(word, char){
-    for(let i=0; i<word.length; i++){
-        if(word[i]===char) return true;   
-    }
-    return false;
-}
-
-
-function init(){
-
-document.getElementById("main_wrapper").style.background="crimson";
-let numbers =document.getElementsByClassName("numBtn");
-Array.sort(numbers);
-console.log(numbers);
-}
-
-function clickBtn(){
-
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML =this.innerHTML :display.innerHTML +=this.innerHTML; 
-}
-
-
-function _1(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML ="1":display.innerHTML +="1";
-    
-}
-function _2(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML ="2":display.innerHTML +="2";
-    
-}
-function _3(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML ="3":display.innerHTML +="3";
-    
-}
-function _4(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML ="4":display.innerHTML +="4";
-    
-}
-function _5(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML ="5":display.innerHTML +="5";
-   
-}
-function _6(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML ="6": display.innerHTML +="6";
-    
-}
-function _7(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML ="7":display.innerHTML +="7";
-    
-}
-function _8(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML +="8":display.innerHTML +="8";
-    
-}
-function _9(){
-    (opp.mode=="result")?opp.clear():null;
-    (display.innerHTML=="0")? display.innerHTML ="9":display.innerHTML +="9";
-    
-    }
-function _0(){
-    (opp.mode=="result")?opp.clear():null;
-    display.innerHTML +="0";
-    
-}
-
-function _dot(){
-    (display.innerHTML=="")?opp.clear(): (checkChar(display.innerHTML,"."))? null: display.innerHTML+=".";
-    opp.mode="typing";
-}
-function _per(){
-    (display.innerHTML=="")?opp.clear():display.innerHTML =  (opp.read()*0.01).toFixed(2);
-}
-
-function _AC(){
+  clear: function () {
     display.innerHTML = "";
-    opp.res=0;
-    opp.first=0;
-    opp.second=0;
-    opp.mode="";
-    opp.sign= "";
+  },
+
+  read: function () {
+    return parseFloat(display.innerHTML);
+  },
+};
+
+function checkChar(word, char) {
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === char) return true;
+  }
+  return false;
 }
 
-function _add(){
-    opp.first = opp.read();
-    opp.mode = "typing";
-    opp.sign= "+";
-    opp.clear();
+// function init() {
+//   document.getElementById("main_wrapper").style.background = "crimson";
+//   let numbers = document.getElementsByClassName("numBtn");
+//   numbers.console.log(numbers);
+// }
+
+function clickDigitBtn(digit) {
+  switch (digit) {
+    case "dot":
+      display.innerHTML == ""
+        ? operation.clear()
+        : checkChar(display.innerHTML, ".")
+        ? null
+        : (display.innerHTML += ".");
+      operation.mode = "typing";
+      break;
+    case "percent":
+      display.innerHTML == ""
+        ? operation.clear()
+        : (display.innerHTML = (operation.read() * 0.01).toFixed(2));
+      break;
+    case "AC":
+      display.innerHTML = "";
+      operation.result = 0;
+      operation.first = 0;
+      operation.second = 0;
+      operation.mode = "";
+      operation.sign = "";
+      break;
+    case "0":
+      operation.mode == "result" ? operation.clear() : null;
+      display.innerHTML == digit ? null : (display.innerHTML += digit);
+      break;
+    default:
+      operation.mode == "result" ? operation.clear() : null;
+      display.innerHTML == toString(digit)
+        ? (display.innerHTML = digit)
+        : (display.innerHTML += digit);
+  }
 }
 
-function _sub(){
-    opp.first = opp.read();
-    opp.mode = "typing";
-    opp.sign= "-";
-    opp.clear();
+function clickOperatorBtn(operator) {
+  operation.mode = "typing";
+  operation.first = operation.read();
+  operation.clear();
+  operation.sign = operator;
 }
 
-function _mul(){
-    opp.first = opp.read();
-    opp.mode = "typing";
-    opp.sign= "*";
-    opp.clear();
-}
+function _result() {
+  if (operation.mode == "result") {
+    operation.result = eval(
+      `operation.result${operation.sign}operation.second`
+    );
+  }
+  if (operation.mode == "typing") {
+    if (display.innerHTML != "") {
+      operation.second = operation.read();
+      operation.result = eval(
+        `${operation.first} ${operation.sign} ${operation.second}`
+      );
+      operation.mode = "result";
+    } else null;
+  }
 
-function _div(){
-    opp.first = opp.read();
-    opp.mode = "typing";
-    opp.sign= "/";
-    opp.clear();
-}
-
-function _res(){
-    if(opp.mode=="result"){
-        if (opp.sign=="+"){
-        opp.res+=opp.second;  
-        }
-        if (opp.sign=="-"){
-            opp.res-=opp.second;  
-            }
-        if (opp.sign=="*"){
-                opp.res*=opp.second;  
-            }
-        if (opp.sign=="/"){
-                opp.res/=opp.second;  
-            }
-    }
-
-    if(opp.mode=="typing"){
-    opp.second = opp.read();
-    opp.res = eval(`${opp.first} ${opp.sign} ${opp.second}`);
-    opp.mode="result";    
-    }
-
- 
-    (display.innerHTML!="")? display.innerHTML = opp.res: null;
-    
+  display.innerHTML != "" ? (display.innerHTML = operation.result) : null;
 }
